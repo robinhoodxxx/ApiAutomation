e95cf5fb33f882bd46e23239d3fac596d4cc1659
robinhoodxxx <karrothutanesh016@gmail.com>
2025-04-12 22:38:52 +0530

    added new extent report version

--- src/main/java/Listners/Reports/ExtentReport.java
+++ src/main/java/Listners/Reports/ExtentReport.java
@@ -3,7 +3,7 @@ package Listners.Reports;
 import Listners.ConfigReader;
 import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.ExtentTest;
-import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
 import com.aventstack.extentreports.reporter.configuration.Theme;
 
 import static Listners.CommonVariables.*;
@@ -11,12 +11,11 @@ import static Listners.DataSheet.TESTCASE_STATUS;
 
 public class ExtentReport {
 
-    String json = "{ \"name\": \"John Doe\", \"age\": 30, \"city\": \"New York\" }";
 
 
     public static ExtentReports getExtent() {
         String path = ConfigReader.get("ExtentReportPath", CONFIG);
-        ExtentHtmlReporter htmlReporter = new ExtentHtmlReporter(path);
+        ExtentSparkReporter htmlReporter = new ExtentSparkReporter(path);
         htmlReporter.config().setDocumentTitle("Test Report");
         htmlReporter.config().setReportName("Automation Test Results");
         htmlReporter.config().setTheme(Theme.STANDARD);
@@ -29,13 +28,15 @@ public class ExtentReport {
 
 
     public static void overall(String status, ExtentTest test) {
+        String val= TESTCASE_STATUS+":"+status;
         switch (status) {
-            case PASS -> test.pass(TESTCASE_STATUS);
-            case FAIL -> test.fail(TESTCASE_STATUS);
-            case SKIP -> test.skip(TESTCASE_STATUS);
-            default -> test.info(TESTCASE_STATUS);
+            case PASS -> test.pass(val);
+            case FAIL -> test.fail(val);
+            case SKIP -> test.skip(val);
+            default -> test.info(val);
         }
     }
+
 
 
     public static void testStatus(String validationName, String status, ExtentTest testNode) {
